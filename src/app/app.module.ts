27f3bacd76fda65cf4bf9ca5e90fc595d88f9b60
Monil Shah <monil@Monils-MacBook-Pro.local>
2018-04-25 08:58:38 +0530

    added tabs navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,30 +6,33 @@ import { BankDepositsComponent } from './bank-deposits/bank-deposits.component';
 
 import { FormsModule,ReactiveFormsModule } from '@angular/forms'; 
 
-import {MatOptionModule,MatSelectModule,MatInputModule,MatFormFieldModule} from '@angular/material';
+import {MatOptionModule,MatSelectModule,MatInputModule,MatFormFieldModule,
+  MatTabsModule} from '@angular/material';
 
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { BankDepositsService } from '../app/bank-deposits.service';
-import {HttpClientModule } from '@angular/common/http'
+import {HttpClientModule } from '@angular/common/http';
+import { NavigationComponent } from './navigation/navigation.component'
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BankDepositsComponent
+    BankDepositsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    MatOptionModule,MatSelectModule,MatFormFieldModule,MatInputModule,
+    MatOptionModule,MatSelectModule,MatFormFieldModule,MatInputModule,MatTabsModule,
     NoopAnimationsModule,
     HttpClientModule
   ],
   exports:[
-    MatOptionModule,MatSelectModule,MatFormFieldModule,MatInputModule
+    MatOptionModule,MatSelectModule,MatFormFieldModule,MatInputModule,MatTabsModule
   ],
   providers: [
     BankDepositsService
